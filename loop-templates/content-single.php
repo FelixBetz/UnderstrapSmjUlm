f59cc5aabebf463ba91e9168c0e6bca4e866acf7
Felix Betz <Betz.Felix@web.de>
2023-01-25 22:59:41 +0100

    remove sidebar from single page

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -12,19 +12,39 @@ defined( 'ABSPATH' ) || exit;
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
 	<header class="entry-header">
+		<div class="row">
+			<div class="col-md-auto" >
+			<img style="width: 100px; border: 1px solid black;"
+			src="<?php echo get_the_post_thumbnail_url( $post->ID, 'thumbnail' ); ?>" />
+				
+			</div>
+			<div class="col">
+				<div class="row">
+					<div class="col"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></div>
+				</div>
+				<div class="row">
+					<div class="col-md-auto">
+					<?php understrap_posted_on(); ?>
+					</div>
+					<div class="col-md-auto">
+						<?php 
+							$categories = get_the_category();
+							if ( ! empty( $categories ) ) {
+								foreach($categories as $category) { 
+									echo '<a style="text-decoration: none;" href="'. get_category_link( $category->term_id)  .'" <span class="m-1 badge badge-pill">' . $category->name . ' </span></a>'; 
+								}
+							}
+						?>
+					</div>
+				</div>
+			</div>
+			
+		</div>
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-
-			<?php understrap_posted_on(); ?>
-
-		</div><!-- .entry-meta -->
+		
 
 	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
+	<hr class="my-3">
 	<div class="entry-content">
 
 		<?php
