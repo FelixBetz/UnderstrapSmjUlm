cecbb8af1a599f62176b42e01b20b48883d7c4b8
Felix Betz <Betz.Felix@web.de>
2023-01-26 11:57:46 +0100

    archive: remove sidebar and display posts in 3 cols

--- archive.php
+++ archive.php
@@ -47,7 +47,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 							* If you want to override this in a child theme, then include a file
 							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
 							*/
-							get_template_part( 'loop-templates/content', get_post_format() );
+							get_template_part( 'loop-templates/content_archive', get_post_format() );
 						}
 					} else {
 						get_template_part( 'loop-templates/content', 'none' );
@@ -62,8 +62,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 			understrap_pagination();
 
 			// Do the right sidebar check and close div#primary.
-			get_template_part( 'global-templates/right-sidebar-check' );
+			//todoFB get_template_part( 'global-templates/right-sidebar-check' );
 			?>
+					<!-- todoFB Do the right sidebar check and close div#primary.-->
+					</div>
 
 		</div><!-- .row -->
 
